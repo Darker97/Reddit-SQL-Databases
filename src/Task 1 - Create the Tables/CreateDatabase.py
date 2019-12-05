c82d53e60ae1cc26c57b41b5f063db99b67f240c
christian@baltzer.de <christian@baltzer.de>
2019-12-05 21:40:32 +0100

    init of The Tablecreator.py

---
+++ src/Task 1 - Create the Tables/CreateDatabase.py
@@ -0,0 +1,60 @@
+import json
+import mysql.connector as mysql
+
+
+def createTables(Assistant):
+    Assistant.execute("show Databases")
+
+
+
+def main():
+    # Load the keys for the MySql Database
+
+    File = open("../Keys/Setup.config").readlines()
+    host = File.pop(0)
+    port = File.pop(0)
+    user = File.pop(0)
+    passwd = File.pop(0)
+    database = File.pop(0)
+
+    print("------------------------------------------------------")
+    print("Connecting to Database: " + host + " as " + user)
+    print("Password: " + passwd)
+    print("------------------------------------------------------")
+
+    Database = mysql.connect(
+        host=host,
+        port=port,
+        user=user,
+        passwd=passwd,
+        database=database
+        # auth_plugin='mysql_native_password'
+    )
+
+    Assistant = Database.cursor()
+
+    createTables(Assistant)
+
+# Load the Data from the Source Folder
+
+
+# Load the Data from the given File
+    for File in Files:
+        datenpunkt = File.readlines()
+        for data in datenpunkt:
+            encoded = json.loads(data)
+            # print (encoded["author"])
+            DataToSQLServer(encoded, Database)
+
+
+
+# ------------------------------------------------ #
+
+
+main()
+# database = JoinDatabase()
+
+
+
+
+
