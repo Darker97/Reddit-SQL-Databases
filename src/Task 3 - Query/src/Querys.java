602c51f1d923cea771ec96eef88a314994b92f99
christian@baltzer.de <christian@baltzer.de>
2019-12-10 13:21:17 +0100

    init Functions

--- src/Task 3 - Query/src/Querys.java
+++ src/Task 3 - Query/src/Querys.java
@@ -1,2 +1,30 @@
 public class Querys {
+
+    public static String SELECT(){
+        return "";
+    }
+    public static String SELECT_SPECIFIC_USER(String USER){
+        return "";
+    }
+    public static String SELECT_COMMENTS_SPECIFIC_SUB(String SUB){
+        return "";
+    }
+    public static String SELECT_COMMENTS_WITH_LOL(){
+        return "";
+    }
+    public static String SELECT_SUB_BY_LINK(String Link){
+        return "";
+    }
+    public static String SELECT_SCORE_BY_USER(String USER){
+        return "";
+    }
+    public static String SELECT_HIGHEST_COMMENTS_BY_SUB(String SUB){
+        return "";
+    }
+    public static String SELECT_CONTACTED_USERS(String USER){
+        return "";
+    }
+    public static String SELECT_USER_ONLY_COMMETED_ONCE(){
+        return "";
+    }
 }
