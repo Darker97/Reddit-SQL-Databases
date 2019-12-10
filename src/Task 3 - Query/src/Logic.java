6cf466e8d25b5569994278689790ea85df77ba6f
elephant12 <caro.brueckmann@web.de>
2019-12-10 14:10:30 +0100

    switch case

--- src/Task 3 - Query/src/Logic.java
+++ src/Task 3 - Query/src/Logic.java
@@ -2,7 +2,20 @@ public class Logic{
 	
   public static void  submittQuery(String specific, int queryType, connector main) {
 	//TEST
-	  System.out.println("test: " + specific + " " + queryType);	  
+	  System.out.println("test: " + specific + " " + queryType);	
+	  
+	  switch(queryType) {
+	  case 1: 
+	  case 2: 	 
+	  case 3:
+	  case 4:
+	  case 5:
+	  case 6:
+	  case 7:
+	  case 8:
+	  }
   }
+  
+  
 
 }
