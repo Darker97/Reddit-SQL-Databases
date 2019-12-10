1f4052fccd527b089372174a8944763f6f1735ae
christian@baltzer.de <christian@baltzer.de>
2019-12-10 14:13:42 +0100

    added comments

--- src/Task 3 - Query/src/Querys.java
+++ src/Task 3 - Query/src/Querys.java
@@ -3,27 +3,73 @@ public class Querys {
     public static String SELECT(String Input){
         return "";
     }
+
+    /**
+     * Task 1
+     * @param USER
+     * @return
+     */
     public static String SELECT_SPECIFIC_USER(String USER){
         return "";
     }
+
+    /**
+     * Task 2
+     * @param SUB
+     * @return
+     */
     public static String SELECT_COMMENTS_SPECIFIC_SUB(String SUB){
         return "";
     }
+
+    /**
+     * Task 3
+     * @return
+     */
     public static String SELECT_COMMENTS_WITH_LOL(){
         return "";
     }
+
+    /**
+     * Task 4
+     * @param Link
+     * @return
+     */
     public static String SELECT_SUB_BY_LINK(String Link){
         return "";
     }
+
+    /**
+     * Task 5
+     * @param USER
+     * @return
+     */
     public static String SELECT_SCORE_BY_USER(String USER){
         return "";
     }
+
+    /**
+     * Task 6
+     * @param SUB
+     * @return
+     */
     public static String SELECT_HIGHEST_COMMENTS_BY_SUB(String SUB){
         return "";
     }
+
+    /**
+     * Task 7
+     * @param USER
+     * @return
+     */
     public static String SELECT_CONTACTED_USERS(String USER){
         return "";
     }
+
+    /**
+     * Task 8
+     * @return
+     */
     public static String SELECT_USER_ONLY_COMMETED_ONCE(){
         return "";
     }
